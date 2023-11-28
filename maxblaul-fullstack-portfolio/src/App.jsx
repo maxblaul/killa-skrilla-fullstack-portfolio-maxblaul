49f0b519fe4df0aff2a4fcbebc29f293239e2f86
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-11-27 23:47:28 -0600

    code overhaul... i messed up

---
+++ maxblaul-fullstack-portfolio/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from '../maxblaul-fullstack-portfolio/src/components/Header';
+import Footer from '../maxblaul-fullstack-portfolio/src/components/Footer';
+import Nav from '../maxblaul-fullstack-portfolio/src/components/Nav';
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+    <Header></Header>
+    <Footer></Footer>
+    <Nav></Nav>
+</div>
+  )
+}
+
+export default App

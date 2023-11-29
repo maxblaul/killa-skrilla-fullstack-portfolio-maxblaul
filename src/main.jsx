f88467f09bcad3bda8f2190b367234a891f9a9fe
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-11-29 16:32:47 -0600

    moving files

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
-import About from './components/About/index.jsx';
-import Portfolio from './components/Portfolio/index.jsx';
-import Resume from './components/Resume/index.jsx';
-import Contact from './components/Contact/index.jsx';
+import About from '../components/About/index.jsx';
+import Portfolio from '../components/Portfolio/index.jsx';
+import Resume from '../components/Resume/index.jsx';
+import Contact from '../components/Contact/index.jsx';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css'
 

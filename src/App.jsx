e08241d508a2538fee874566c573d2c13b7ceef0
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-11-29 16:54:23 -0600

    commented out unneccesary code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from '../assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import Header from '../maxblaul-fullstack-portfolio/src/components/Header';
 import Footer from '../maxblaul-fullstack-portfolio/src/components/Footer';

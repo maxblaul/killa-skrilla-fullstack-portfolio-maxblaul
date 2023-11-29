acfd9fc678665ed0a5822a738f2b7b1ebef5399f
maxblaul <144166736+maxblaul@users.noreply.github.com>
2023-11-29 17:10:27 -0600

    paths

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react'
 // import reactLogo from '../assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
-import Header from '../maxblaul-fullstack-portfolio/src/components/Header';
-import Footer from '../maxblaul-fullstack-portfolio/src/components/Footer';
-import Nav from '../maxblaul-fullstack-portfolio/src/components/Nav';
+import Header from '../src/components/Header';
+import Footer from '../src/components/Footer';
+import Nav from '../src/components/Nav';
 
 
 function App() {
